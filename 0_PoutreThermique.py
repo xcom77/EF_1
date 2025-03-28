984c9f2f45f09ee5c4b16def236381c490da70c7
jules <juju.fayol@gmail.com>
2025-03-28 11:54:46 +0100

    feat : cours 2

---
+++ 0_PoutreThermique.py
@@ -0,0 +1,71 @@
+# @author: renaudf
+
+import numpy as np
+import FE_library as mef
+
+# ==================================================
+# Parameters of the thermal problem
+# ==================================================
+a = 0.005    # Thickness of the beam [m]
+L = 0.1      # Total length of the beam [m]
+Ta = 25      # Ambient temperature [°C]
+Lbda = 60    # Thermal conductivity [W/m/°C]
+h = 10       # Convective loss coefficient [W/m^2/°C]
+q0 = 2000    # Heat flow source [W/m^2]
+
+# ==================================================
+# Analytical solution of the problem
+# ==================================================
+
+
+# Initialization of the analytical model
+theo = mef.ThermalBeam_th(a, Ta, Lbda, h, q0)
+
+# Discretization of the beam
+x_theo = np.linspace(0, L, 100)
+
+# Problem resolution
+theo.solve(x_theo)
+
+# Plot of the analytical solution of the temperature
+fig = mef.Fig_temp()
+fig.plot(theo, label="Analytical", color="k")
+fig.legend()
+
+
+# ===================================================
+# Solution with a single linear element
+# ===================================================
+
+fe_1el_lin = mef.ThermalBeam_fe_1el_lin(a, Ta, Lbda, h, q0)
+fe_1el_lin.solve(L)
+fe_1el_lin.postprocess(x_theo)
+fig.plot(fe_1el_lin, label="1 linear elem.", color="r")
+fig.legend()
+
+
+
+# ================================================================
+# Solution with several linear elements
+# ================================================================
+
+fe_Nel_lin = mef.ThermalBeam_fe_Nel_lin(a, Ta, Lbda, h, q0)
+
+# 4 elements and 5 nodes
+fe_Nel_lin.mesh(L, 5)
+fe_Nel_lin.solve()
+fe_Nel_lin.postprocess()
+
+fig.plot(fe_Nel_lin, label="4 linear elem.", color="b",
+         marker='o', markerfacecolor='none', markersize=3)
+fig.legend(fontsize=8)
+
+
+# 12 elements and 13 nodes
+fe_Nel_lin.mesh(L, 13)
+fe_Nel_lin.solve()
+fe_Nel_lin.postprocess()
+fig.plot(fe_Nel_lin, label="12 linear elem.", color=[0, 0.8, 0],
+         marker='o', markerfacecolor='none', markersize=3)
+fig.legend(fontsize=8)
+fig.show()
--- FE_library.py
+++ FE_library.py
@@ -62,3 +62,100 @@ class Fig_temp:
     
     def show(self, **kwargs):
         plt.show()
+
+class ThermalBeam_fe_1el_lin:
+    def __init__(self, a, Ta, Lbda, h, q0):
+        self.a = a          # Thickness of the beam [m]
+        self.Ta = Ta        # Ambient temperature [°C]
+        self.Lbda = Lbda    # Thermal conductivity [W/m/°C]
+        self.h = h          # Convective loss coefficient [W/m²/°C]
+        self.q0 = q0        # Heat flow source [W/m²]
+
+    def solve(self, L):
+        self.L = L
+        # Matrices
+        M1 = self.Lbda / self.L * np.array([[1, -1], [-1, 1]])
+        M2 = self.h * np.array([[0, 0], [0, 1]])
+        M3 = 4/6 * self.h * self.L / self.a * np.array([[2, 1], [1, 2]])
+        # Vectors
+        V1 = 4/2 * self.h * self.L * self.Ta / self.a * np.array([1, 1])
+        V2 = self.q0 * np.array([1, 0])
+        V3 = self.h * self.Ta * np.array([0, 1])
+        # Résolution
+        self.fe_M = M1 + M2 + M3
+        self.fe_V = V1 + V2 + V3
+        self.fe_Tn = np.linalg.solve(self.fe_M, self.fe_V)
+
+    def postprocess(self, x):
+        self.x = x
+        # Polynomial basis evaluated at x
+        P = np.ones((len(x), 2))
+        P[:, 1] = x
+        # Temperature interpolation
+        L = self.L
+        self.T = P @ (np.array([[1, 0], [-1/L, 1/L]]) @ self.fe_Tn)
+        # Derivative of the polynomial basis evaluated at x
+        P_x = np.zeros((len(x), 2))
+        P_x[:, 1] = 1
+        self.q = - self.Lbda * P_x @ (np.array([[1, 0], [-1/L, 1/L]]) @ self.fe_Tn)
+
+class ThermalBeam_fe_Nel_lin:
+    def __init__(self, a, Ta, Lbda, h, q0):
+        self.a = a
+        self.Ta = Ta
+        self.Lbda = Lbda
+        self.h = h
+        self.q0 = q0
+
+    def mesh(self, L, N):
+        self.L = L
+        self.xn = np.linspace(0, L, N)
+        self.le = L / (N - 1)
+
+    def solve(self):
+        # Matrices
+        M1 = self.Lbda / self.le * np.array([[1, -1], [-1, 1]])
+        M3 = 4/6 * self.h * self.le / self.a * np.array([[2, 1], [1, 2]])
+        
+        # Vectors
+        V1 = 4/2 * self.h * self.le * self.Ta / self.a * np.array([1, 1])
+        
+        # Matrix initialization
+        Nb_nodes = len(self.xn)
+        Nb_elem = Nb_nodes - 1
+        self.fe_M = np.zeros((Nb_nodes, Nb_nodes))
+        self.fe_V = np.zeros(Nb_nodes)
+
+        # Matrix assembly for each element
+        for ind in range(0, Nb_elem):
+            indices = ind + np.array([0, 1])
+            self.fe_M[np.ix_(indices, indices)] += M1 + M3
+            self.fe_V[np.ix_(indices)] += V1
+
+        # 2nd term ==> h TL*TL
+        self.fe_M[-1, -1] += self.h
+
+        # 5th term ==> T0 * q0
+        self.fe_V[0] += self.q0
+
+        # 6th term ==> h TL * TA
+        self.fe_V[-1] += self.h * self.Ta
+
+        # Resolution
+        self.fe_Tn = np.linalg.solve(self.fe_M, self.fe_V)
+
+    def postprocess(self):
+        # Vector initialization
+        Nb_nodes = len(self.xn)
+        Nb_elem = Nb_nodes - 1
+        self.x = np.zeros(2 * Nb_elem)
+        self.T = np.zeros(2 * Nb_elem)
+        self.q = np.zeros(2 * Nb_elem)
+
+        # Postprocessing of the temperature and heat flow
+        for ind in range(0, Nb_elem):
+            indices = ind + np.array([0, 1])
+            ind_stock = 2 * ind + np.array([0, 1])
+            self.x[ind_stock] = self.xn[indices]
+            self.T[ind_stock] = self.fe_Tn[indices]
+            self.q[ind_stock] = -self.Lbda * np.ones(2) * np.diff(self.fe_Tn[indices]) / self.le
